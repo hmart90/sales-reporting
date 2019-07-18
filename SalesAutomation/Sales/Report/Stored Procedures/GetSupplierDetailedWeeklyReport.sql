1e77b2a4ab3a0a4925c32a5da1bf7169ee367106
hmart90 <h.martinaa@gmail.com>
2019-07-18 22:11:56 +0200

    combined report

---
+++ SalesAutomation/Sales/Report/Stored Procedures/GetSupplierDetailedWeeklyReport.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [Report].[GetSubSupplierDetailedWeeklyReport]
+	@StartDate DATE,
+	@EndDate DATE,
+	@SubSupplierId INT
+AS
+
+SELECT pm.[TPN]
+      ,pm.[EAN]
+      ,pm.Category
+      ,ss.[Name] as SubSupplierName
+      ,pm.TitleHU
+	  ,st.Code
+	  ,st.[Name] as StoreName
+	  ,soldunits.EventDate
+	  ,ISNULL(soldunits.Number,0) AS SoldUnitsTotal
+	 
+
+FROM [dbo].[Product] as pm
+
+INNER JOIN dbo.SubSupplier as ss on ss.SubSupplierId = pm.SubSupplierId
+
+LEFT OUTER JOIN (
+SELECT ProductId
+		  ,StoreId
+		  ,EventDate
+		  ,Number
+	FROM MSTR.Sales
+	WHERE	EventDate >= @StartDate
+			and EventDate <= @EndDate
+) AS soldunits ON pm.ProductId = soldunits.ProductId 
+
+inner join [dbo].[Store] as st on st.StoreId = soldunits.StoreId
+
+where	pm.SubSupplierId = @SubSupplierId OR @SubSupplierId IS NULL
+		  
+ORDER BY
+		pm.[TPN]
+      ,pm.[EAN]
+      ,pm.Category
+      ,ss.[Name]
+      ,pm.TitleHU
+
+RETURN 0
