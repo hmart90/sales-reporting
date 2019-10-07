7d00ceda7ca9c7c9cf76c698cb921e1bc7329980
hmart90 <h.martinaa@gmail.com>
2019-10-07 22:33:55 +0200

    tesco and subsupplier billing reports

---
+++ SalesAutomation/Sales/Report/Stored Procedures/GetSubSupplierBillingReport.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [Report].[GetSubSupplierBillingReport]
+	@StartDate DATE,
+	@EndDate DATE,
+	@SubSupplierId INT
+AS
+
+SELECT 
+		p.TPN
+		,p.EAN
+		,p.TitleHU as [Description]
+      ,sum(s.[Number]) as SumSales
+	  ,round(pr.SupplierRetailPrice,0) as SupplierRetailPrice
+	  ,round(pr.SupplierCostPrice,0) as SupplierCostPrice
+	  ,st.SumStock
+
+FROM [FR].[Sales] as s
+INNER JOIN dbo.Product as p on p.ProductId = s.ProductId
+INNER JOIN dbo.Price as pr on pr.ProductId = p.ProductId AND pr.StartDate <= s.EventDate AND (pr.EndDate > s.EventDate OR pr.EndDate IS NULL)
+LEFT OUTER JOIN (
+	SELECT st.ProductId, SUM(st.[Number]) as SumStock
+	FRoM FR.StockClosing as st
+	WHERE st.EventDate = @EndDate
+	GROUP BY st.ProductId
+) as st on st.ProductId = s.ProductId 
+
+WHERE s.[EventDate] >= @StartDate AND s.[EventDate] <= @EndDate
+		AND s.[Number] != 0
+		AND p.SubSupplierId = @SubSupplierId
+
+GROUP BY p.TPN
+		,p.EAN
+		,p.TitleHU
+		  ,pr.SupplierRetailPrice
+		  ,pr.SupplierCostPrice
+		  ,st.SumStock
+
+ORDER BY p.TPN asc
+
+RETURN 0
---
+++ SalesAutomation/Sales/Report/Stored Procedures/GetTescoBillingReport.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [Report].[GetTescoBillingReport]
+	@StartDate DATE,
+	@EndDate DATE
+AS
+
+SELECT 
+		p.TPN
+		,p.EAN
+		,p.TitleHU as [Description]
+      ,sum(s.[Number]) as SumSales
+	  ,round(pr.TescoRetailPrice,0) as SupplierRetailPrice
+	  ,round(pr.TescoCostPrice,0) as SupplierCostPrice
+	  ,st.SumStock
+
+FROM [FR].[Sales] as s
+INNER JOIN dbo.Product as p on p.ProductId = s.ProductId
+INNER JOIN dbo.Price as pr on pr.ProductId = p.ProductId AND pr.StartDate <= s.EventDate AND (pr.EndDate > s.EventDate OR pr.EndDate IS NULL)
+LEFT OUTER JOIN (
+	SELECT st.ProductId, SUM(st.[Number]) as SumStock
+	FRoM FR.StockClosing as st
+	WHERE st.EventDate = @EndDate
+	GROUP BY st.ProductId
+) as st on st.ProductId = s.ProductId 
+
+WHERE s.[EventDate] >= @StartDate AND s.[EventDate] <= @EndDate
+		AND s.[Number] != 0
+
+GROUP BY p.TPN
+		,p.EAN
+		,p.TitleHU
+		  ,pr.TescoCostPrice
+		  ,pr.TescoRetailPrice
+		  ,st.SumStock
+
+ORDER BY p.TPN asc
+
+
+RETURN 0
